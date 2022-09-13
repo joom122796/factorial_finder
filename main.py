b5171c18aab613ede5c24aaa5d7024afa875e1ea
joom122796 <113173627+joom122796@users.noreply.github.com>
2022-09-13 10:35:29 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+num = int(input('Enter a number into the factorial finder: '))
+
+def factorial(number):
+  factorial = 1
+  while number < 0:
+    number = int(input('Factorials do not exist for negative numbers. \nEnter a positive number into the factorial finder: '))
+  else:
+    if number == 0:
+      print('The factorial of 0 is 1')
+    else:
+      for x in range(1,number+1):
+        factorial = factorial*x
+      print(f'The factorial of {number} is {factorial}')
+
+
+factorial(num)
